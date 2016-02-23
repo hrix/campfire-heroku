8160bd1f99eec1c20e3035038c0d7e258ee0dacd
bohdankutkovy <kutkovybohdan@gmail.com>
2016-02-23 15:03:41 +0200

    welcome controller and view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   root to: 'rooms#show'
+
+  authenticated :user do
+    root 'rooms#show', as: "authenticated_root"
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
